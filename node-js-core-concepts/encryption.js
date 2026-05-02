8cd9e398c341f7b1d19cfefdf2a13c4e7a5b8c1a
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2026-05-02 22:33:28 +0600

    Add encryption and decryption functionality using AES-256-CBC

---
+++ node-js-core-concepts/encryption.js
@@ -0,0 +1,36 @@
+const crypto = require("crypto");
+
+const algorithm = "aes-256-cbc";
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+function encrypt(text) {
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(text, "utf-8", "hex");
+  encrypted += cipher.final("hex");
+
+  return {
+    iv: iv.toString("hex"),
+    encryptedData: encrypted,
+  };
+}
+
+function decrypt(encrypted, ivHex) {
+  const decipher = crypto.createDecipheriv(
+    algorithm,
+    key,
+    Buffer.from(ivHex, "hex"),
+  );
+  let decrypted = decipher.update(encrypted, "hex", "utf-8");
+  decrypted += decipher.final("utf-8");
+
+  return decrypted;
+}
+
+console.log("Encryption Data: ");
+const sensitiveData = "My credit card: 4242 4242 4242";
+console.log("Original Data: ", sensitiveData);
+const encrypted = encrypt(sensitiveData);
+console.log("Encrypted: ", encrypted);
+console.log("decrypted: ", decrypt(encrypted.encryptedData, encrypted.iv));
