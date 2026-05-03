dc4236bb480fcd5ca390c7b787f9fe0f8fb2be44
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2026-05-03 20:12:33 +0600

    feat: add PostgreSQL database connection and user/todo table initialization

--- server-with-express/src/server.ts
+++ server-with-express/src/server.ts
@@ -1,9 +1,57 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
 const app = express();
 const port = 5000;
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// database connection Postgres
+const pool = new Pool({
+  connectionString: `${process.env.CONNECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(150) UNIQUE NOT NULL,
+      age INT,
+      phone VARCHAR(15),
+      address TEXT,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+    )
+    `);
+
+  //TODOS TABLE
+  await pool.query(`
+      CREATE TABLE IF NOT EXISTS todos(
+      id SERIAL PRIMARY KEY,
+      user_id INT REFERENCES users(id) ON DELETE CASCADE,
+      title VARCHAR(200) NOT NULL,
+      description TEXT,
+      completed BOOLEAN DEFAULT false,
+      due_date DATE,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW() 
+      )
+    `);
+};
+
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello Next Level Developers");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(200).json({ success: true, message: "API is working" });
 });
 
 app.listen(port, () => {
