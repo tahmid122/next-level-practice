5f8189f1c458b8c94cff0f40d0cd028b6555a416
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2026-05-13 21:23:47 +0600

    feat: add database initialization and todos table creation

---
+++ todo-app/src/config/db.ts
@@ -0,0 +1,34 @@
+import { Pool } from "pg";
+import config from ".";
+
+const pool = new Pool({
+  connectionString: config.db_url,
+});
+
+// database query
+
+export const initDB = async () => {
+  // users table
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+        )
+    `);
+
+  //todos table
+  await pool.query(`
+            CREATE TABLE IF NOT EXISTS todos(
+                id SERIAL PRIMARY KEY,
+                user_id INT REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE,
+                title VARCHAR(200) NOT NULL,
+                description TEXT,
+                isCompleted BOOLEAN DEFAULT false,
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+        `);
+};
--- todo-app/src/server.ts
+++ todo-app/src/server.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import config from "./config";
+import { initDB } from "./config/db";
 const app = express();
 const PORT = config.port;
 //middleware
 app.use(express.json());
+
+// Initialize db
+
+initDB();
+
+// default
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Welcome to <span style="color:blue">TODO APP</span> Server</h1>`,
+  );
+});
 
 // listen/connection
 
